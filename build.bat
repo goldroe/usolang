09e0695dbb7f481e2076796b2bba16d72514c086
Carlos Rivera <cr4185320@gmail.com>
2022-11-15 00:24:44 -0600

    Added files to build

--- build.bat
+++ build.bat
@@ -2,9 +2,9 @@
 
 set WarningFlags=-WX -W4 -wd4100 -wd4189 -wd4200 -wd4530
 set CompilerFlags=-FC -GR- -EHa- -nologo -Zi %WarningFlags%
-set SourceFiles=..\src\main.cpp
+set SourceFiles=..\src\main.cc ..\src\lexer.cc  ..\src\tests.cc ..\src\common.cc ..\src\ast.cc
 
 if not exist bin mkdir bin
 pushd bin
-cl %CompilerFlags% %SourceFiles% -FeUso.exe
+cl %CompilerFlags% %SourceFiles% -Feuso.exe
 popd
