c33d4f4a43689fd9ee673789a18c027409f871b2
Carlos Rivera <cr4185320@gmail.com>
2022-11-16 22:52:56 -0600

    More ast stuff

--- src/ast.h
+++ src/ast.h
@@ -16,15 +16,16 @@ struct Typespec {
     
 };
 
+
 struct FuncParam {
     char *identifier;
-    Typespec type;
+    Typespec *type;
 };
 
 struct Aggregate_Field {
     char *identifier;
-    Typespec type;
-    Expr expr;
+    Typespec *type;
+    Expr *expr;
 };
 
 struct Func_Decl {
@@ -33,7 +34,7 @@ struct Func_Decl {
 
 struct Enum_Field {
     char *identifier;
-    Expr expr;
+    Expr *expr;
 };
 
 struct Enum_Decl {
@@ -50,8 +51,8 @@ struct Union_Decl {
 
 struct Var_Decl {
     char *identifier;
-    Typespec type;
-    Expr expr;
+    Typespec *type;
+    Expr *expr;
 };
 
 struct Decl {
@@ -59,30 +60,87 @@ struct Decl {
     char *identifier;
     union {
         Func_Decl funcdecl;
-        Enum_Decl enumdecl;
-        Struct_Decl structdecl;
-        Union_Decl uniondecl;
-        Var_Decl vardecl;
+        Enum_Decl *enumdecl;
+        Struct_Decl *structdecl;
+        Union_Decl *uniondecl;
+        Var_Decl *vardecl;
     };
 };
 
 enum Stmt_Type {
     STMT_NONE,
     STMT_IF,
+    STMT_DO,
     STMT_WHILE,
     STMT_FOR,
-    STMT_ASSIGNMENT,
-    STMT_CALL,
+    STMT_SWITCH,
+    STMT_RETURN,
+    STMT_BREAK,
+    STMT_CONTINUE,
+    STMT_ASSIGN,
+    STMT_AUTO_ASSIGN,
+    STMT_EXPR,
     STMT_BLOCK,
+};
+
+struct Stmt {
+    Stmt_Type type;
+    Expr *expr;
+    union {
+        Stmt *statements;
+        Expr *rhs;
+    };
 };
 
 enum Expr_Type {
     EXPR_NONE,
-    EXPR_BINARY,
-    EXPR_TYPECAST,
-    EXPR_UNARY,
-    EXPR_OPERAND,
+    EXPR_INT,
+    EXPR_FLOAT,
+    EXPR_STR,
+    EXPR_IDENT,
+    EXPR_CAST,
     EXPR_CALL,
+    EXPR_INDEX,
+    EXPR_FIELD,
+    EXPR_COMPOUND,
+    EXPR_UNARY,
+    EXPR_BINARY,
+    EXPR_TERNARY,
 };
 
+struct Expr {
+    Expr_Type type;
+    
+    union {
+        uint64_t int_val;
+        double float_val;
+        char *str_val;
+        char *identifier;
+        
+        struct {
+            Typespec *compund_type;
+            Expr **compound_list;
+        };
+        // unary
+        struct {
+            Expr *operand;
+            union {
+                Expr **args;
+                Expr *index;
+                const char *field;
+            };
+        };
+        // binary
+        struct {
+            Expr *left;
+            Expr *right;
+        };
+        // ternary
+        struct {
+            Expr *cond;
+            Expr *then_expr;
+            Expr *else_expr;
+        };
+    };
+};
 #endif
