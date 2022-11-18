29012f51514d6a027547ba3df2e87909ba34d3e1
Carlos Rivera <cr4185320@gmail.com>
2022-11-18 16:10:41 -0600

    Added warning flag

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set WarningFlags=-WX -W4 -wd4100 -wd4189 -wd4200 -wd4530
+set WarningFlags=-WX -W4 -wd4100 -wd4189 -wd4200 -wd4530 -wd4201
 set CompilerFlags=-FC -GR- -EHa- -nologo -Zi %WarningFlags%
 set SourceFiles=..\src\main.cc ..\src\lexer.cc  ..\src\tests.cc ..\src\common.cc ..\src\ast.cc
 
